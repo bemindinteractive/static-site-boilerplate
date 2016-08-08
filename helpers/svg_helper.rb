d74120e2e3dddfa592a0aa083f1537904fbce49c
mattia.astorino <astorino.design@gmail.com>
2016-08-08 16:04:58 +0200

    chore: Update icon helper

--- helpers/svg_helper.rb
+++ helpers/svg_helper.rb
@@ -1,7 +1,7 @@
 module SvgIcon
   def icon(name)
     root = Middleman::Application.root
-    file_path = "#{root}/source/images/icons/#{name}.svg"
+    file_path = "#{root}/source/assets/icons/#{name}.svg"
     return File.read(file_path) if File.exists?(file_path)
     '(not found)'
   end
